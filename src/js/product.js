574b046d10363b0bc73ad694f91397b4784ffc9a
Godfrey Hogan Eyo <geyo2@byupathway.edu>
2025-11-06 10:29:09 +0100

    fixed add item to cart error

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = getLocalStorage("so-cart");
+
+  // If nothing is stored OR the stored value is not an array, reset it
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  // Add the new product
+  cartItems.push(product);
+
+  // Save back to local storage
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
