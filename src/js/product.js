7bab49b014309b2a1a3db94244a74e70364858cc
BYUI-Student2081190 <cscott1@byupathway.edu>
2025-11-06 14:35:46 -0700

    Completed Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const curCart = getLocalStorage("so-cart") || [];
-  curCart.push(product);
-  setLocalStorage("so-cart", curCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Get the product details
+const product = new ProductDetails(productId, dataSource);
+product.init();
