83cc44b9e61f3f72022eb5a35f7a15f9d9ae0579
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-11-13 22:23:21 +0100

    week3 team14 activity - dynamic header and footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,7 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import {loadHeaderFooter} from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
@@ -8,3 +9,6 @@ const productId = getParam("product");
 // Get the product details
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
+// Load header and footer
+loadHeaderFooter();
