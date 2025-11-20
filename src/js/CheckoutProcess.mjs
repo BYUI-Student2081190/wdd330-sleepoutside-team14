696b7881418de8b8ea1751f659c381b94074ac8d
BYUI-Student2081190 <cscott1@byupathway.edu>
2025-11-20 14:10:56 -0700

    First half

---
+++ src/js/CheckoutProcess.mjs
@@ -0,0 +1,56 @@
+import { getLocalStorage } from "./utils.mjs";
+
+export default class CheckoutProcess {
+    
+    constructor(key, outputSelector) {
+        this.key = key;
+        this.outputSelector = outputSelector;
+        this.list = [];
+        this.itemTotal = 0;
+        this.shipping = 0;
+        this.tax = 0;
+        this.orderTotal = 0;
+    }
+
+    init() {
+        this.list = getLocalStorage(this.key);
+        this.calculateItemSummary();
+    }
+
+    calcItemSubTotal() {
+        // calculate and display the total amount of the items in the cart, and the number of items.
+        const summaryElement = document.querySelector(
+        this.outputSelector + " #cartTotal"
+        );
+        const itemNumElement = document.querySelector(
+        this.outputSelector + " #num-items"
+        );
+        itemNumElement.innerText = this.list.length;
+        // calculate the total of all the items in the cart
+        const amounts = this.list.map((item) => item.FinalPrice);
+        this.itemTotal = amounts.reduce((sum, item) => sum + item);
+        summaryElement.innerText = `$${this.itemTotal}`;;
+    }
+
+    calcOrderTotal() {
+        this.tax = (this.itemTotal * 0.06);
+        this.shipping = 10 + (this.list.length - 1) * 2;
+        this.orderTotal = (
+            parseFloat(this.itemTotal) +
+            parseFloat(this.tax) +
+            parseFloat(this.shipping)
+        );
+        this.displayOrderTotals();
+    }
+
+    displayOrderTotals() {
+        // once the totals are all calculated display them in the order summary page
+        const tax = document.querySelector(`${this.outputSelector} #tax`);
+        const shipping = document.querySelector(`${this.outputSelector} #shipping`);
+        const orderTotal = document.querySelector(`${this.outputSelector} #orderTotal`);
+
+        tax.innerText = `$${this.tax.toFixed(2)}`;
+        shipping.innerText = `$${this.shipping.toFixed(2)}`;
+        orderTotal.innerText = `$${this.orderTotal.toFixed(2)}`;
+    }
+}
