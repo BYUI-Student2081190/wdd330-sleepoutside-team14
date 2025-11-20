7b6cd3eb89fdf03d571a73875bb0177167b687dc
BYUI-Student2081190 <cscott1@byupathway.edu>
2025-11-20 15:44:26 -0700

    Fixed the merge request errors

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,5 +1,5 @@
 // Import renderListWithTemplate
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate, setLocalStorage } from "./utils.mjs";
 
 // Card Template
 function cartItemTemplate(item) {
@@ -17,7 +17,7 @@ function cartItemTemplate(item) {
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: ${item.Quantity}</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <p class="cart-card__price">$${item.FinalPrice.toFixed(2)}</p>
   </li>`;
 
   return newItem;
@@ -30,22 +30,72 @@ export default class ShoppingCart {
     // Save the cart and listElements
     this.cart = cart; // The cart is what is in local storage
     this.listElement = listElement; // The list element is the HTML element we want it rendered in
+    this.cartTotal = 0; // Store the cart total
+
+    // Call the calculate total function
+    this.calculateTotal();
   }
 
   init() {
     // Create a check to see if the cart is empty, we do this with a falsy value
-    if (!this.cart) {
+    if (this.cart.length <= 0) {
       this.renderCartContentsEmpty();
     } else {
       // The cart is not empty
       // Call the renderCartContents Function
       this.renderCartContents(this.cart);
+      // Add the buttons code here
+      
     }
+  }
+
+  calculateTotal() {
+    // Calculate total
+    let total = 0;
+    if (this.cart.length > 0)
+    {
+      this.cart.forEach(element => {
+          total += element.Quantity * element.FinalPrice;
+      });
+      this.cartTotal = total;
+    } else {
+      this.cartTotal = total;
+    }
+    // Set the total
+    document.querySelector(".list-total").textContent = `Total: $${this.cartTotal.toFixed(2)}`;
+  }
+
+  removeItem(index) {
+    // Remove item from cart array
+    this.cart.splice(index, 1);
+
+    // Update localStorage
+    setLocalStorage("so-cart", this.cart);
+
+    // Re-render cart
+    if (this.cart.length > 0) {
+      this.renderCartContents(this.cart);
+    } else {
+      this.renderCartContentsEmpty();
+    };
+    // Re-render the total for the user
+    this.calculateTotal();
+  }
+
+  addDeleteButtonsCode() {
+    // Attach remove button listeners
+    this.listElement.querySelectorAll(".cartBtn").forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        this.removeItem(index);
+      });
+    });
   }
 
   renderCartContents(cartItems) {
     // Now pass the items into the util function to render them
     renderListWithTemplate(cartItemTemplate, this.listElement, cartItems, "afterbegin", true);
+    // Now add the deleteButtonsCode
+    this.addDeleteButtonsCode();
   }
 
   renderCartContentsEmpty() {
